3502698329ca03f47597cae18dcaea7aaf46765c
Jon Cram <webignition@gmail.com>
2024-01-05 15:59:30 +0000

    Create storage error

---
+++ src/Error/StorageErrorInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ServiceRequest\Error;
+
+/**
+ * @phpstan-type SerializedStorageError array{
+ *    class: 'storage',
+ *    type: ?non-empty-string,
+ *    location: ?non-empty-string,
+ *    object_type: non-empty-string,
+ *    context: array<mixed>
+ *  }
+ */
+interface StorageErrorInterface extends ErrorInterface
+{
+    public const ERROR_CLASS = 'storage';
+
+    /**
+     * @return ?non-empty-string
+     */
+    public function getLocation(): ?string;
+
+    /**
+     * @return array<string, scalar>
+     */
+    public function getContext(): array;
+
+    /**
+     * @return non-empty-string
+     */
+    public function getObjectType(): string;
+
+    /**
+     * @return SerializedStorageError
+     */
+    public function serialize(): array;
+}
