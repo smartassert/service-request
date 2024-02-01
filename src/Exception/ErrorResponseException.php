0b5d8d58149b5561ff33dc6ec3f43eeae9ff97e8
Jon Cram <webignition@gmail.com>
2024-02-01 10:09:11 +0000

    Import sources ErrorResponseException

---
+++ src/Exception/ErrorResponseException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ServiceRequest\Exception;
+
+use SmartAssert\ServiceRequest\Error\ErrorInterface;
+
+class ErrorResponseException extends \Exception
+{
+    /**
+     * @param int<400, 599> $code
+     */
+    public function __construct(
+        public readonly ErrorInterface $error,
+        int $code,
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct('', $code, $previous);
+    }
+}
